2c6936742792701f1f14116c3e9050764da8a454
tamae-r <tamae.mob@gmail.com>
2023-01-30 11:33:09 -0300

    2.alteracao: CRUD completo

--- Projeto2301/src/application/TelaFXMLController.java
+++ Projeto2301/src/application/TelaFXMLController.java
@@ -17,32 +17,33 @@ public class TelaFXMLController {
 	private TextField txtId;
 	@FXML
 	private TextField txtNome;
-	
+
 	@FXML
-	private void initialize () {}
-	
+	private void initialize() {
+	}
+
 	@FXML
-	public void actionMensagem (ActionEvent event) {
+	public void actionTexto(ActionEvent event) {
 		id.setText(txtId.getText());
 		nome.setText(txtNome.getText());
 	}
+
 	@FXML
-	public void actionSQLSelect (ActionEvent event) {
+	public void actionSQLSelect(ActionEvent event) {
 		try {
 			DBUtil db = DBUtil.getInstance();
 			PreparedStatement ps = db.getConnection().prepareStatement("Select * from aluno");
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
-				System.out.println(rs.getInt("id")+"-"+rs.getString("nome"));
+				System.out.println(rs.getInt("id") + "-" + rs.getString("nome"));
 			}
 		} catch (Exception e) {
-			System.out.println("Erro: "+e.toString());
+			System.out.println("Erro: " + e.toString());
 		}
-		
 	}
-	
+
 	@FXML
-	public void actionSQLInsert (ActionEvent event) {
+	public void actionSQLInsert(ActionEvent event) {
 		try {
 			DBUtil db = DBUtil.getInstance();
 			PreparedStatement ps = db.getConnection().prepareStatement("Insert into aluno (id, nome) values (?, ?)");
@@ -50,8 +51,32 @@ public class TelaFXMLController {
 			ps.setString(2, txtNome.getText());
 			ps.execute();
 		} catch (Exception e) {
-			System.out.println("Erro: "+e.toString());
+			System.out.println("Erro: " + e.toString());
 		}
 	}
 
+	@FXML
+	public void actionSQLDelete(ActionEvent event) {
+		try {
+			DBUtil db = DBUtil.getInstance();
+			PreparedStatement ps = db.getConnection().prepareStatement("Delete from aluno where id = ?");
+			ps.setInt(1, Integer.parseInt(txtId.getText()));
+			ps.execute();
+		} catch (Exception e) {
+			System.out.println("Erro: " + e.toString());
+		}
+	}
+
+	@FXML
+	public void actionSQLUpdate(ActionEvent event) {
+		try {
+			DBUtil db = DBUtil.getInstance();
+			PreparedStatement ps = db.getConnection().prepareStatement("update aluno set nome = ? where id = ?");
+			ps.setString(1, txtNome.getText());
+			ps.setInt(2, Integer.parseInt(txtId.getText()));
+			ps.execute();
+		} catch (Exception e) {
+			System.out.println("Erro: " + e.toString());
+		}
+	}
 }
